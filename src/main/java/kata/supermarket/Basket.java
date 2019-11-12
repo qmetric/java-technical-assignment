16d1dda022bd3a382ce4c37b67158c065e3bd0f9
dwood <dwood@qmetric.co.uk>
2019-11-12 10:12:35 +0000

    Renamed Basket -> Basket to indicate the intended limits of its responsibility. Added TODO comment on Basket.discounts() to highlight where this could interact with something to provide that functionality.

--- src/main/java/kata/supermarket/Basket.java
+++ src/main/java/kata/supermarket/Basket.java
@@ -22,13 +22,13 @@ public class Basket {
     }
 
     public BigDecimal total() {
-        return new Calculator().calculateTotal();
+        return new TotalCalculator().calculate();
     }
 
-    private class Calculator {
+    private class TotalCalculator {
         private final List<Item> items;
 
-        Calculator() {
+        TotalCalculator() {
             this.items = items();
         }
 
@@ -39,11 +39,18 @@ public class Basket {
                     .setScale(2, RoundingMode.HALF_UP);
         }
 
+        /**
+         * TODO: This could be a good place to apply the results of
+         *  the discount calculations.
+         *  It is not likely to be the best place to do those calculations.
+         *  Think about how Basket could interact with something
+         *  which provides that functionality.
+         */
         private BigDecimal discounts() {
             return BigDecimal.ZERO;
         }
 
-        private BigDecimal calculateTotal() {
+        private BigDecimal calculate() {
             return subtotal().subtract(discounts());
         }
     }
